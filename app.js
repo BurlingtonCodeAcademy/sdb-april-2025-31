994831bf7d34e556422f6019dc8329f3505f840e
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-02 17:59:01 -0500

    connected to mongodb

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("happy wednesday")
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+const MONGO = process.env.MONGODB;
+
+mongoose.connect(`${MONGO}/myMongooseTest`)
+
+const db = mongoose.connection
+
+db.once("open", () => {
+    console.log(`connected: ${MONGO}`)
+})
+
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log("server is running")
+})
